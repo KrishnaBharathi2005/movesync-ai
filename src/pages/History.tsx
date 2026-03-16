874b3576f087e915750e25bc5ff9a114d37fcaac
KrishnaBharathi Koduru <bharathikoduru2526@gmail.com>
2026-03-16 20:12:16 +0530

    Connected EmotionHistory component to History page

---
+++ src/pages/History.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import EmotionHistory from "../components/EmotionHistory";
+
+export default function History() {
+  return (
+    <div className="flex w-full h-full">
+      <EmotionHistory />
+    </div>
+  );
+}
