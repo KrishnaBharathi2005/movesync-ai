98981790de0479b5405282cb8d8f1979fbbf05e3
KrishnaBharathi Koduru <bharathikoduru2526@gmail.com>
2026-03-16 20:34:56 +0530

    Connected EmotionHistory dashboard to history page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -3,7 +3,7 @@ import EmotionHistory from "../components/EmotionHistory";
 
 export default function History() {
   return (
-    <div className="flex w-full h-full">
+    <div className="w-full h-full">
       <EmotionHistory />
     </div>
   );
